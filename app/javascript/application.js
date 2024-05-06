4bcd730214ec3b0d3570612964aaf28c44a7c51e
Julian <cjk@rabidaudio.com>
2024-05-06 14:53:58 -0400

    create artists

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Entry point for the build script in your package.json
 import '@hotwired/turbo-rails'
 import './controllers'
+
+import BulmaTagsInput from '@creativebulma/bulma-tagsinput'
+
+window.addEventListener('DOMContentLoaded', () => BulmaTagsInput.attach())
