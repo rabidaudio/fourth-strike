6e26f3165f86c0bd76850c580bfa49d7598316c0
Julian <cjk@rabidaudio.com>
2024-07-26 16:34:51 -0400

    fix docker build

--- config/initializers/chrome.rb
+++ config/initializers/chrome.rb
@@ -1,6 +1,8 @@
-if Rails.env.production?
+if ENV['CHROME_URL'].present?
   # In production the chrome browser is running in a separate container,
   # so we need to set up the appropriate config settings.
+  # Chrome only supports IP or localhost hostnames, so we need to lookup
+  # the IP address of the container.
   Rails.application.config.ferrum_browser = {
     url: ENV['CHROME_URL'].sub('chrome', Resolv.getaddress('chrome'))
   }
