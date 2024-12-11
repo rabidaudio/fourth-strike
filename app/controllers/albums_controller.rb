47ba7128e488bfe050d1d63ba6af793554a5ace7
Julian <cjk@rabidaudio.com>
2024-12-11 12:56:45 -0500

    tracks editor

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -17,12 +17,106 @@ class AlbumsController < ApplicationController
 
   def extract_bandcamp_details
     render json: {
-      name: 'THE GARAGES SIGN OFF @ DESERT BUS 2024',
-      artist_name: 'the garages',
-      album_art_url: 'https://f4.bcbits.com/img/a0623417941_16.jpg',
-      bandcamp_price: { cents: 666, currency: 'USD' },
-      release_date: '2024-11-13',
-      bandcamp_id: '827202132'
+      album: {
+        name: 'THE GARAGES SIGN OFF @ DESERT BUS 2024',
+        artist_name: 'the garages',
+        album_art_url: 'https://f4.bcbits.com/img/a0623417941_16.jpg',
+        bandcamp_price: { cents: 666, currency: 'USD' },
+        release_date: '2024-11-13',
+        bandcamp_id: '827202132'
+      },
+      tracks: [
+        {
+          bandcamp_url: 'https://thegarages.bandcamp.com/track/bones-to-ohio-anyone-else-live-db2024',
+          bandcamp_id: '3812987682',
+          name: 'bones to ohio (anyone else) [live @ db2024]',
+          track_number: 1,
+          lyrics: <<~LYRICS,
+            i wanna be anyone
+            i wanna be anyone anyone else
+            anyone else but me
+
+            give it up for a thicker shell
+            i wanna be anyone anyone else
+            so what’s it gonna be?
+
+            don’t care if it hurts like hell
+            i just wanna tear away every sticky cell
+            that feels anything anything anything like myself
+
+            don’t need anybody’s help
+            i’m gonna be anyone anyone else
+
+            yeah the shell is strong but the body’s weak
+            you gotta learn how to yell before you learn how to speak
+            it’s all bitter work with your face in the dirt
+            do you think it’ll sting do you think it’ll hurt
+            i want the burning taste of infinity
+            put the scales in my eyes where everybody can see
+            i just wanna be ok
+            and i don’t wanna be myself today
+
+            nothing left to call home
+            half a season to be alone, be alone again
+            what’s it take to leave
+
+            my skin keeps getting tight
+            i need someone to dig me out dig me out tonight
+            and learn how to breathe
+
+            nothing left in this desert town
+            gonna burn it all burn it all burn it all down
+            so what’s it gonna be?
+
+            calling out for a god to help
+            i’m gonna be anyone anyone else
+
+            yeah the shell is strong but the body’s weak
+            you gotta learn how to yell before you learn how to speak
+            it’s all bitter work with your face in the dirt
+            do you think it’ll sting do you think it’ll hurt
+            i want the burning taste of infinity
+            put the scales in my eyes where everybody can see
+            i just wanna be ok
+            i just wanna be ok
+
+            i left a decent life for a step outside
+            when it all comes down, will we be alright
+            (hold on to me, darling, hold on to me)
+            i swear i’m on a knife’s edge waiting to fall for anything
+            i don’t care what the future brings
+            i just gotta get away from the rest of me
+            (hold on to me, darling, hold on to me)
+            i don’t wanna be dead, i just wanna be free
+            i don’t wanna be dead, i just wanna be free
+
+            yeah the shell is strong but the body’s weak
+            you gotta learn how to yell before you learn how to speak
+            it’s all bitter work with your face in the dirt
+            do you think it’ll sting do you think it’ll hurt
+            i want the burning taste of infinity
+            put the scales in my eyes where everybody can see
+            i’m getting sick and tired of the shape i’m in
+            do you think it’ll fail, do you think it’s a sin
+            as i disappear in the hole in the sky
+            was it worth all the pain? was it all just a lie?
+            my body's getting sore
+            and i'm not gonna be myself anymore
+          LYRICS
+          credits: <<~CREDITS
+            written by max (noise-land.bandcamp.com)
+            originally on short circuit 02
+            lead vocals by riley (yuppiesupper.bandcamp.com, @thwackamabob on tumblr and twitter)
+            backing vocals by vigilant baker (vigilantbaker.bandcamp.com)
+            lead guitar by max (noise-land.bandcamp.com)
+            rhythm guitar by rolo (jenandrolo.bandcamp.com) and raph (apha.bandcamp.com, twitter.com/aphamusic)
+            drums by hibi (hibi.bandcamp.com, twitter.com/hibiscuitss)
+            bass by jennifer cat (jenandrolo.bandcamp.com)
+            sax by rosie (thornsband.bandcamp.com, @rosie.drown on instagram)
+            gang vocals by em (vigilanceandgrace.bandcamp.com), pitch (imperfectpitch.bandcamp.com), cliff, astrid, ras (ebhband.bandcamp.com), and tegan (tonalchroma.bandcamp.com)#{' '}
+          CREDITS
+        }
+      ]
     }
   end
 
@@ -45,7 +139,8 @@ class AlbumsController < ApplicationController
     @album = Album.new(artist_name: 'the garages')
     # restore_changes!(@album)
     @props = {
-      album: @album
+      album: @album,
+      tracks: []
     }
   end
 
--- app/javascript/components/AlbumForm.vue
+++ app/javascript/components/AlbumForm.vue
@@ -1,14 +1,17 @@
 <script setup>
-  import { ref, reactive } from 'vue'
+  import { ref, reactive, computed } from 'vue'
 
   import { fetch } from '../rails'
   import { extract_bandcamp_details_path } from '../routes'
 
   import MoneyField from './MoneyField.vue'
   
-  const props = defineProps(['album'])
+  const props = defineProps(['album', 'tracks'])
 
-  const album = reactive(props.album)
+  const album = reactive(props.album || {})
+  const tracks = ref(props.tracks || [])
+
+  const hidden = computed(() => tracks.value.map(t => t.track_number == null))
 
   async function loadDataFromBandcamp() {
     if (!album.bandcamp_url.match(/^(https?:\/\/)?([a-z0-9-]+\.)?bandcamp.com/)) return
@@ -17,12 +20,42 @@
     const res = await fetch(extract_bandcamp_details_path({url: album.bandcamp_url}), {method: "POST"})
     const params = await res.json()
 
-    Object.assign(album, params)
+    Object.assign(album, params.album)
+    tracks.value = params.tracks
   }
 
   function makePrivateIfUnreleased() {
     if (!album.release_date) return
     album.private = new Date(album.release_date) > new Date()
+  }
+
+  function addTrack() {
+    tracks.value.push({ track_number: tracks.value.length + 1 })
+    renumberTracks()
+  }
+
+  function toggleHiddenTrackAt(index) {
+    if (tracks.value[index].track_number) {
+      tracks.value[index].track_number = null
+    } else {
+      tracks.value[index].track_number = index + 1
+    }
+    renumberTracks()
+  }
+
+  function removeTrackAt(index) {
+    tracks.value.splice(index, 1)
+    renumberTracks()
+  }
+
+  function renumberTracks() {
+    let track_number = 1
+    for (let idx in tracks.value) {
+      if (tracks.value[idx].track_number) {
+        tracks.value[idx].track_number = track_number
+        track_number += 1
+      }
+    }
   }
 </script>
 
@@ -36,78 +69,188 @@
 
 <!-- NOTE: assumes already in a form -->
 <template>
-  <div class="field">
-    <label class="label">Bandcamp URL</label>
-    <div class="control">
-      <input class="input" name="album[bandcamp_url]" type="url" placeholder="https://theband.bandcamp.com/album/the-album" v-model="album.bandcamp_url" @change="loadDataFromBandcamp">
-    </div>
-  </div>
 
-  <div class="field">
-    <label class="label">Album Name</label>
-    <div class="control">
-      <input class="input" name="album[name]" type="text" v-model="album.name">
-    </div>
-  </div>
-
-  <div class="field">
-    <label class="label">Artist Name</label>
-    <div class="control">
-      <input class="input" name="album[artist_name]" type="text" v-model="album.artist_name">
-    </div>
-  </div>
-
-  <input type="hidden" name="album[bandcamp_id]" v-model="album.bandcamp_id">
-
-  <div class="field">
-    <label class="label">Price on Bandcamp</label>
-    <div class="control">
-      <MoneyField v-model="album.bandcamp_price" name="album[bandcamp_price]" />
-    </div>
-  </div>
-
-  <div class="field">
-    <label class="label">Album Art URL</label>
-    <div class="control">
-      <input class="input" name="album[album_art_url]" type="text" v-model="album.album_art_url">
-    </div>
-  </div>
-
-  <img class="album-art" v-bind:src="album.album_art_url" v-if="album.album_art_url"/>
-
-  <div class="field">
-    <label class="label">Release Date</label>
-    <div class="control">
-      <input class="input" name="album[release_date]" type="date" v-model="album.release_date" @change="makePrivateIfUnreleased">
-    </div>
-  </div>
-
-  <div class="field">
-    <label class="label">Catalog Number</label>
-    <div class="control">
-      <input class="input" name="album[catalog_number]" type="text" v-model="album.catalog_number" placeholder="ABC-001">
-    </div>
-  </div>
-
-  <div class="field">
-    <label class="label">UPCs</label>
-    <div class="control">
-      <input class="input" name="album[upcs]" type="tags" v-model="album.upcs" placeholder="0123456789000">
-    </div>
-  </div>
-
-  <div class="field">
-    <div class="control">
-      <label class="checkbox">
-        <input type="checkbox" name="album[private]" v-model="album.private" />
-        <span> Hidden</span>
-      </label>
-    </div>
-    <p class="help">
-        Visible to admins only. Useful for unreleased albums or ones taken down.
+  <section class="section">
+    <div class="field">
+      <label class="label is-required">Bandcamp URL</label>
+      <div class="control">
+        <input class="input" name="album[bandcamp_url]" type="url" placeholder="https://theband.bandcamp.com/album/the-album" v-model="album.bandcamp_url" @change="loadDataFromBandcamp">
+      </div>
+      <p class="help">
+        Populating this will autofill much of the form from Bandcamp.
       </p>
-  </div>
+    </div>
 
-  <!-- TODO: tracks -->
-  
+    <div class="field">
+      <label class="label is-required">Album Name</label>
+      <div class="control">
+        <input class="input" name="album[name]" type="text" v-model="album.name">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label is-required">Artist Name</label>
+      <div class="control">
+        <input class="input" name="album[artist_name]" type="text" v-model="album.artist_name">
+      </div>
+    </div>
+
+    <input type="hidden" name="album[bandcamp_id]" v-model="album.bandcamp_id">
+
+    <div class="field">
+      <label class="label">Price on Bandcamp</label>
+      <div class="control">
+        <MoneyField v-model="album.bandcamp_price" name="album[bandcamp_price]" />
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Album Art URL</label>
+      <div class="control">
+        <input class="input" name="album[album_art_url]" type="text" v-model="album.album_art_url">
+      </div>
+    </div>
+
+    <img class="album-art" :src="album.album_art_url" v-if="album.album_art_url"/>
+
+    <div class="field">
+      <label class="label is-required">Release Date</label>
+      <div class="control">
+        <input class="input" name="album[release_date]" type="date" v-model="album.release_date" @change="makePrivateIfUnreleased">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label is-required">Catalog Number</label>
+      <div class="control">
+        <input class="input" name="album[catalog_number]" type="text" v-model="album.catalog_number" placeholder="ABC-001">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">UPCs</label>
+      <div class="control">
+        <input class="input" name="album[upcs]" type="tags" v-model="album.upcs" placeholder="0123456789000">
+      </div>
+    </div>
+
+    <div class="field">
+      <div class="control">
+        <label class="checkbox">
+          <input type="checkbox" name="album[private]" v-model="album.private" />
+          <span> Hidden</span>
+        </label>
+      </div>
+      <p class="help">
+        Visible to admins only. Useful for albums that are unreleased, have been taken down, or are no longer associated with the label.
+      </p>
+    </div>
+  </section>
+
+
+  <section class="section">
+    <h2 class="is-size-3">Tracks</h2>
+
+    <div v-for="(track, index) in tracks">
+      <h4>
+        <span>Track </span>
+        <span v-if="track.track_number">{{ track.track_number }}</span>
+        <span v-else>[Hidden]</span>
+        <span>&nbsp;</span>
+        <button @click.prevent="removeTrackAt(index)"><i class="ri-delete-bin-6-line"></i></button>
+      </h4>
+
+      <div class="field is-horizontal">
+        <div class="field-label">
+          <label class="label">Hidden</label>
+        </div>
+        <div class="field-body">
+          <div class="field is-narrow">
+            <div class="control">
+              <label class="checkbox">
+                <input type="checkbox" :checked="hidden[index]" @change="toggleHiddenTrackAt(index)" />
+              </label>
+            </div>
+            <p class="help">
+              Visible to admins only. Useful for tracks that have been taken down.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label is-required">Name</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input" name="album[tracks][][name]" type="text" v-model="track.name" placeholder="Song name">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label is-required">Bandcamp URL</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input class="input" name="album[tracks][][bandcamp_url]" type="url" placeholder="https://theband.bandcamp.com/track/the-song" v-model="track.bandcamp_url">
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label is-required">ISRC</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control is-expanded">
+              <input class="input" name="album[tracks][][isrc]" type="text" v-model="track.isrc" placeholder="QZZ123456789">
+            </div>
+            <p class="help">
+              From DistroKid. This is necessary to attribute streaming revenue.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <input type="hidden" name="album[tracks][][bandcamp_id]" v-model="track.bandcamp_id">
+      <input type="hidden" name="album[tracks][][track_number]" v-model="track.track_number">
+
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Credits</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <textarea class="textarea" name="album[tracks][][credits]" v-model="track.credits"></textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Lyrics</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <textarea class="textarea" name="album[tracks][][lyrics]" v-model="track.lyrics"></textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div><!-- v-for -->
+
+    <button class="button" @click.prevent="addTrack">Add Track</button>
+  </section>
 </template>
