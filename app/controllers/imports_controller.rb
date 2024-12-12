51239b0954813c3f8fdc6eea3f3bfaf10658b840
Julian <cjk@rabidaudio.com>
2024-12-11 23:47:57 -0500

    refactor

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -6,17 +6,19 @@ class ImportsController < ApplicationController
   def index; end
 
   def import_bandcamp_sales
-    path = Rails.root.join('storage/exports', params[:report].original_filename)
-    File.binwrite(path, params[:report].read)
-    LoadBandcampJob.perform_later(path.to_s)
-    flash[:success] = 'Loading report. The data will update over the next couple of minutes'
-    redirect_to imports_path
+    load_report(LoadBandcampJob)
   end
 
   def import_distrokid_streams
+    load_report(LoadDistrokidJob)
+  end
+
+  private
+
+  def load_report(job_class)
     path = Rails.root.join('storage/exports', params[:report].original_filename)
     File.binwrite(path, params[:report].read)
-    LoadDistrokidJob.perform_later(path.to_s)
+    job_class.perform_later(path.to_s)
     flash[:success] = 'Loading report. The data will update over the next couple of minutes'
     redirect_to imports_path
   end
